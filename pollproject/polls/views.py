765842a7e36018a5df1d65421fde6c37db18385a
Atiemo-Keseku David Obeng <datiemokeseku16@gmail.com>
2025-10-30 15:56:06 +0000

    Create views and urls.py

--- pollproject/polls/views.py
+++ pollproject/polls/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
+def index(request):
+    latest_question = Question.objects.all()
+    output = ', '.join(q.question_text for q in latest_question)
+    return HttpResponse(output)
